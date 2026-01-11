e802b5e943cd959b12db1e0f136a018340d8f873
Destiny Christian Church <destinychristianchurchintl@gmail.com>
2026-01-11 12:28:04 +0100

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+export default function Home (){
+
+
+return(
+<div className="hero">
+<div className="black-cover">
+    <div className="home-details">
+        <img src="/images/church-pfp.png" alt="" />
+    <h1>Destiny Christian Church Int'l</h1>
+    <p>...changing the World with the Word</p>
+    </div>
+</div>
+</div>
+
+)
+    
+
+}
