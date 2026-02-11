9c56eb8d7ec3e428f33f45e5c0b394f4bc262ceb
Ukattah-Divine603 <ukattahdivine603@gmail.com>
2026-02-12 00:07:50 +0100

    Updated till About

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Welcome from "./Welcome";
+import About from "./AboutUs";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/welcome" element={<Welcome />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
